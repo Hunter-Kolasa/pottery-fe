26283bad774f0cb04fd07314ecbce5fe1ece7f98
Hunter Kolasa <73991568+Hunter-Kolasa@users.noreply.github.com>
2023-03-15 20:19:28 -0400

    dynamic add works

--- src/app/components/tiles-list/tiles-list.component.ts
+++ src/app/components/tiles-list/tiles-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TileService } from 'src/app/services/tile.service';
-import { tap, Observable } from 'rxjs';
+import { tap, Observable, pipe } from 'rxjs';
 
 @Component({
   selector: 'app-tiles-list',
@@ -13,22 +13,29 @@ export class TilesListComponent implements OnInit{
   constructor(private service:TileService) {}
 
   ngOnInit(): void {
-      this.service.getAll()
-        .subscribe(response => {
-          this.tiles = response
-          console.log(this.tiles)
-          // this.service.setTiles(this.tiles)
-        })
+    this.service.getAll().subscribe((res) => {
+      console.log('getAll returned ',  res)
+      // this.tiles = res
+      this.service.setTiles(res)
+    })
+    this.service.getTiles().subscribe((res) => {
+      this.tiles = res
+      console.log('Response recieved from getTiles')
+    })
   }
 
   buyIt(id: any) {
     console.log(id)
     console.log("You bought it! Your item: ", this.tiles.filter(t => t.id == id)[0].title)
+    this.service.getTiles().subscribe(response => {
+      this.tiles=response
+    })
   }
 
   deleteTile(id: any) {
     this.service.delete(id).subscribe(()=> {
       this.tiles = this.tiles.filter((t: any) => t.id != id)
+      // this.service.setTiles(this.tiles.filter((t: any) => t.id != id))
     });
   }
 }
--- src/app/services/tile.service.ts
+++ src/app/services/tile.service.ts
@@ -1,29 +1,38 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map, Subject } from 'rxjs';
-
+import { BehaviorSubject } from 'rxjs'
+import { ASTWithSource } from '@angular/compiler';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class TileService implements OnInit{
-  private _tiles: Subject<any>
+export class TileService{
+  private _tiles: BehaviorSubject<any>
   private baseUrl = 'http://localhost:8080/api/tiles';
-  constructor(private httpClient: HttpClient) { }
-
-  ngOnInit(): void {
-      // this.getAll().subscribe(res => {
-      //   this.setTiles(res)
-      // })
+  constructor(private httpClient: HttpClient) {
+    this._tiles = new BehaviorSubject([])
   }
+
+  // ngOnInit(): void {
+  //     // this.getAll().subscribe(res => {
+  //     //   this.setTiles(res)
+  //     //   console.log('getAll triggered')
+  //     // })
+
+  // }
   getAll() {
     // console.log("In GetAll tile.service.ts")
     return this.httpClient.get(this.baseUrl)
   };
 
-  setTiles(tiles: any): void {
-    this._tiles.next(tiles)
+  setTiles = (tiles: any): void => {
+    console.log('setting _tiles to: ', tiles)
+    if (tiles.length) {
+      console.log('setting _tile', tiles)
+      this._tiles.next(tiles)
+    }
   }
 
   getTiles(): Observable<any> {
