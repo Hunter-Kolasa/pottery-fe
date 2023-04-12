e1a175ea4af3208876f8dd11850d00cff4782a89
Hunter Kolasa <73991568+Hunter-Kolasa@users.noreply.github.com>
2023-04-11 20:35:27 -0400

    Relocate login/reg + btn->dialog
    Relocated the login and register forms. Now are dialogs that pop up from buttons build into header bar

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { MatDialog, MatDialogConfig, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+
+export class RegisterComponent {
+  constructor(public dialog:MatDialog) {}
+
+  openDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.data = {
+    }
+    const dialogRef = this.dialog.open(RegisterDialog)
+  }
+}
+
+@Component({
+  selector: 'register-dialog',
+  templateUrl: 'register-dialog.html'
+})
+
+export class RegisterDialog  implements OnInit {
   form: any = {
     username: null,
     email: null,
