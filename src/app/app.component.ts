6a00a2d4e1e29dc8bc906a77f9a8a59cec401a72
Hunter Kolasa <73991568+Hunter-Kolasa@users.noreply.github.com>
2023-05-01 19:38:24 -0400

    Login/Register/Logout btn logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { TokenStorageService } from './services/token-storage.service';
 
 
 @Component({
@@ -7,14 +8,18 @@ import { FormControl } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pottery-fe';
   page_styles = ['Light','Dark'];
   current_style = this.page_styles[1];
   opened = false;
+  loggedIn: boolean;
 
+  constructor(private tokenService: TokenStorageService) { }
 
-
+  ngOnInit(): void {
+    this.loggedIn = Boolean(window.sessionStorage.getItem('auth-user'));
+  }
   // For light/dark style toggle
   togglePageStyle(): void {
     console.log('Page style was" ', this.current_style)
@@ -24,6 +29,8 @@ export class AppComponent {
     console.log(document.body)
     document.body.classList.toggle('dark-theme');
   }
-
-
+  signOut() {
+    this.tokenService.signOut()
+    window.location.reload()
+  }
 }
